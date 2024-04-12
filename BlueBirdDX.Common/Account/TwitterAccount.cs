cb95428da17aabeead69d81ab4474ac899268cf2
OatmealDome <julian@oatmealdome.me>
2024-04-12 19:39:50 -0400

    TwitterAccount: Add class to hold Twitter API information

---
+++ BlueBirdDX.Common/Account/TwitterAccount.cs
@@ -0,0 +1,33 @@
+namespace BlueBirdDX.Common.Account;
+
+public class TwitterAccount
+{
+    public string ConsumerKey
+    {
+        get;
+        set;
+    } = "";
+
+    public string ConsumerSecret
+    {
+        get;
+        set;
+    } = "";
+
+    public string AccessToken
+    {
+        get;
+        set;
+    } = "";
+
+    public string AccessTokenSecret
+    {
+        get;
+        set;
+    } = "";
+
+    public TwitterAccount()
+    {
+        //
+    }
+}
