ce7ca094dac4a835196729a4fafd1f0f4321a73a
OatmealDome <julian@oatmealdome.me>
2024-04-16 00:15:29 -0400

    PostThreadItem: Add property for a list of attached media

--- BlueBirdDX.Common/Post/PostThreadItem.cs
+++ BlueBirdDX.Common/Post/PostThreadItem.cs
@@ -1,8 +1,16 @@
+using MongoDB.Bson;
+
 namespace BlueBirdDX.Common.Post;
 
 public class PostThreadItem
 {
     public string Text
+    {
+        get;
+        set;
+    }
+
+    public List<ObjectId> AttachedMedia
     {
         get;
         set;
