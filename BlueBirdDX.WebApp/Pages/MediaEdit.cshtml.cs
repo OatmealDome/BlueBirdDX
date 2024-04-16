75e72224ed044b1791c6d12f75154c907b0a0ffb
OatmealDome <julian@oatmealdome.me>
2024-04-16 13:19:48 -0400

    MediaEdit: Add page to edit name and alt text

---
+++ BlueBirdDX.WebApp/Pages/MediaEdit.cshtml.cs
@@ -0,0 +1,54 @@
+using BlueBirdDX.Common.Media;
+using BlueBirdDX.Common.Post;
+using BlueBirdDX.WebApp.Api;
+using BlueBirdDX.WebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace BlueBirdDX.WebApp.Pages;
+
+public class MediaEditModel : PageModel
+{
+    private readonly DatabaseService _database;
+
+    public string MediaId
+    {
+        get;
+        set;
+    }
+    
+    public UploadedMediaApi ApiMedia
+    {
+        get;
+        set;
+    }
+
+    public MediaEditModel(DatabaseService databaseService)
+    {
+        _database = databaseService;
+    }
+    
+    public IActionResult OnGet(string mediaId)
+    {
+        MediaId = mediaId;
+
+        if (!ObjectId.TryParse(mediaId, out ObjectId objectId))
+        {
+            return NotFound();
+        }
+
+        UploadedMedia? realMedia =
+            _database.UploadedMediaCollection.AsQueryable().SingleOrDefault(m => m._id == objectId);
+
+        if (realMedia == null)
+        {
+            return NotFound();
+        }
+        
+        ApiMedia = new UploadedMediaApi(realMedia);
+
+        return Page();
+    }
+}
