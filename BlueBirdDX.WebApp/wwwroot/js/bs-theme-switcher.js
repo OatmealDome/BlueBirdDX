82494a87c264edc6e0775d5c65b2d502076c7bda
OatmealDome <julian@oatmealdome.me>
2024-04-23 13:58:24 -0400

    bs-theme-switcher: Add stripped version of Bootstrap's light/dark theme switcher

---
+++ BlueBirdDX.WebApp/wwwroot/js/bs-theme-switcher.js
@@ -0,0 +1,19 @@
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2024 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
+(() => {
+    'use strict'
+
+    const setTheme = () => {
+        document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+    }
+
+    setTheme()
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        setTheme()
+    })
+})()
