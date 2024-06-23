7780f66512d6122f08bc2e168f3f705f6e18b7a8
OatmealDome <julian@oatmealdome.me>
2024-06-23 18:53:56 -0400

    AccountGroupManager: Implement token refreshing for Threads accounts

--- BlueBirdDX/Social/AccountGroupManager.cs
+++ BlueBirdDX/Social/AccountGroupManager.cs
@@ -2,6 +2,8 @@ using BlueBirdDX.Common.Account;
 using BlueBirdDX.Database;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using OatmealDome.Unravel;
+using OatmealDome.Unravel.Authentication;
 using Serilog;
 using Serilog.Core;
 
@@ -36,6 +38,59 @@ public class AccountGroupManager
         try
         {
             return _accountGroupCollection.AsQueryable().FirstOrDefault(a => a._id == id)!;
+        }
+        finally
+        {
+            _semaphoreSlim.Release();
+        }
+    }
+
+    public async Task RefreshThreadsTokens()
+    {
+        await _semaphoreSlim.WaitAsync();
+
+        try
+        {
+            foreach (AccountGroup group in _accountGroupCollection.AsQueryable().ToList())
+            {
+                ThreadsAccount? account = group.Threads;
+            
+                if (account == null)
+                {
+                    continue;
+                }
+
+                TimeSpan span = account.Expiry - DateTime.UtcNow;
+
+                // Skip tokens that still have more than 7 days left.
+                if (span.TotalDays > 7.0d)
+                {
+                    continue;
+                }
+                
+                LogContext.Information("Refreshing Threads token for account group {groupId}", group._id);
+
+                ThreadsClient client = new ThreadsClient(account.ClientId, account.ClientSecret)
+                {
+                    Credentials = new ThreadsCredentials()
+                    {
+                        CredentialType = ThreadsCredentialType.LongLived,
+                        AccessToken = account.AccessToken,
+                        Expiry = account.Expiry,
+                        UserId = account.UserId
+                    }
+                };
+                
+                ThreadsCredentials credentials = await client.Auth_RefreshLongLivedAccessToken();
+
+                account.AccessToken = credentials.AccessToken;
+                account.Expiry = credentials.Expiry;
+
+                await _accountGroupCollection.ReplaceOneAsync(Builders<AccountGroup>.Filter.Eq(g => g._id, group._id),
+                    group);
+
+                LogContext.Information("Threads token for account group {groupId} refreshed", group._id);
+            }
         }
         finally
         {
