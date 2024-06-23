725c70fa426acf8443663690dace4d64b70f5573
OatmealDome <julian@oatmealdome.me>
2024-06-23 15:34:08 -0400

    BlueskyRef: Add class to hold Bluesky strong references

---
+++ BlueBirdDX.Common/Util/BlueskyRef.cs
@@ -0,0 +1,16 @@
+namespace BlueBirdDX.Common.Util;
+
+public class BlueskyRef
+{
+    public string Uri
+    {
+        get;
+        set;
+    }
+
+    public string Cid
+    {
+        get;
+        set;
+    }
+}
